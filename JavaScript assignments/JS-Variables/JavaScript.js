9eaad8298dd021c2312d3506a48730f1086566be
themaxsi <pcordes.pcc@gmail.com>
2020-09-07 10:57:34 +0200

    done with js varibles

---
+++ JavaScript assignments/JS-Variables/JavaScript.js
@@ -0,0 +1,61 @@
+var greeting = "hello world";
+
+var firstName = "John";
+
+var middleName = "F.";
+
+var lastName = "Kennedy";
+
+var fullName = firstName + " " + middleName + " " + lastName;
+
+console.log(fullName);
+
+//-----------------------
+
+var salary = 5000;
+
+console.log(salary);
+
+//----------------------
+
+var number = (5 + 5) * 3;
+
+console.log(number);
+
+//-------------------
+
+var num1 = 42;
+
+var num2 = 10;
+
+var sum1 = num1 + num2;
+
+console.log(sum1);
+
+//--------------------------
+
+var Deci1 = 2.25;
+
+var Deci2 = 1.44;
+
+var roundDown = Deci1 + Deci2;
+
+console.log(Math.round(roundDown));
+
+//--------------------------------
+
+var square = 65;
+
+console.log(Math.sqrt(square));
+
+//-------------------------------
+
+var remove = "John Doe is 25 years old"
+
+console.log(remove.replace(" Doe" , ""));
+
+//---------------------------------------
+
+var tORf = 2 < 1;
+
+console.log(tORf);
