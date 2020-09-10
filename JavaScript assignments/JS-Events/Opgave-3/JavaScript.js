35f573922ae1e40061143d0874e6b5f42b7927ca
themaxsi <pcordes.pcc@gmail.com>
2020-09-10 14:01:39 +0200

    done with JS-events 3

---
+++ JavaScript assignments/JS-Events/Opgave-3/JavaScript.js
@@ -0,0 +1,7 @@
+var but = document.querySelector('button');
+var box = document.querySelector('div')
+but.addEventListener("click", function()
+    {
+        box.style.backgroundColor = 'yellow'
+    }
+)
