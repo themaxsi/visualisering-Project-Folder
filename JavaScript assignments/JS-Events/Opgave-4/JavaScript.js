e56b36ef344e4aea289bbe5f9664a7df68cd79e2
themaxsi <pcordes.pcc@gmail.com>
2020-09-14 09:30:54 +0200

    not done with JS-event 4

--- JavaScript assignments/JS-Events/Opgave-4/JavaScript.js
+++ JavaScript assignments/JS-Events/Opgave-4/JavaScript.js
@@ -1,9 +1,19 @@
-var but1 = document.getElementById('#1');
-var but2 = document.getElementById('#2');
-var but3 = document.getElementById('#3');
-var but4 = document.getElementById('#4');
-but.addEventListener("click", function()
-    {
+// var but1 = document.getElementById('1');
+// var but2 = document.getElementById('2');
+// var but3 = document.getElementById('3');
+// var but4 = document.getElementById('4');
+
+var BGC = document.querySelector('body')
+
+var buttons = document.querySelectorAll('button');
+buttons.forEach(element => {
+    element.addEventListener('click', () =>{
         
-    }
-)
+        
+
+
+    })
+
+
+});
+
