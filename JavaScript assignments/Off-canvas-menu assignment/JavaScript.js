62c7a5999e52de8b14281c23a8f697cd4386b30d
themaxsi <pcordes.pcc@gmail.com>
2020-09-14 10:43:27 +0200

    done with assigmnet number 23 still need extra assignment on it tho

---
+++ JavaScript assignments/Off-canvas-menu assignment/JavaScript.js
@@ -0,0 +1,17 @@
+var button = document.querySelector('#menu-button');
+
+button.addEventListener('click', function()
+{
+    var menu = document.querySelector('.menu');
+
+    if(menu.classList.contains('show'))
+    {
+        menu.classList.remove("show");
+        button.style.backgroundImage = 'url(menu-white-18dp.svg)';
+    }
+    else
+    {
+        menu.classList.add('show');
+        button.style.backgroundImage = 'url(close-white-18dp.svg)';
+    }
+})
