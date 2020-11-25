f6c7db11c058457d87d9587e7e22abfb9c3743cd
themaxsi <pcordes.pcc@gmail.com>
2020-11-25 14:13:18 +0100

    done with assignment 9 and done a little on my cms project

--- V3.1-Forløb/PHP/Opgave 1/index.php
+++ V3.1-Forløb/PHP/Opgave 1/index.php
@@ -1,3 +1,32 @@
+<?php 
+$minChangeColor = date('i');            //<?php echo $minChangeColor
+$monthForCheck = date('m');             //<?php echo $monthForCheck
+
+function checkNumber($num)
+{
+    if($num % 2 == 0)
+    {
+        return "dark";
+    }
+    else
+    {
+        return "light";
+    }
+}
+function CheckMonths($month)
+{
+    if($month <= 6)
+    {
+        return "GameOver-Zone.png";
+    }
+    else
+    {
+        return "BannerPic2.jpg";
+    }
+}
+
+
+?>
 <!doctype html>
 <html lang="en">
 	<head>
@@ -6,21 +35,32 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>GameOver Zone</title>
 		<link type="text/css" rel="stylesheet" href="style.css">
-	</head>
-	<body class="Container">
-      
+	 </head>
+    <!-- <body class="Container <?php echo checkNumber($minChangeColor)?> "> used to change background color depending on if the min is even or odd  -->
+    <body class="container">
     <header>
+    <img class="icon" src="images/user-icon.svg" alt="user icon">
+    <img class="icon" src="images/facebook-icon.svg" alt="Facebook icon">
+    <img class="icon" src="images/instagram-icon.svg" alt="instagram icon">
+    <img class="icon" src="images/twitter-icon.svg" alt="twitter icon">
+    <img class="icon" src="images/shopping-cart-icon.svg" alt="shopping cart icon">
 
+    <!-- <img src="images/<?php echo CheckMonths($monthForCheck)?>" alt="banner/logo"> -->
 
+    
 
-        <nav>
-            
-        </nav>
+        <!-- <nav class="Area-nav">
+            <ul>
+                <li><a href="#1">My account</a></li>
+                <li><a href="#2">shopping cart</a></li>
+                <li><a href="#3">info</a></li>
+            </ul>
+        </nav> -->
     </header>
         
     <main>
         <section>
-
+        
         </section>
     </main>
 
